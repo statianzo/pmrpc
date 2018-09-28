e355f73a2339bd40c2444ad7a23de396fc1c846b
Jason Staten <jason.staten@domo.com>
2018-09-28 10:49:02 -0600

    add umd build

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,11 @@ const pkg = require('./package.json');
 
 export default {
   input: 'src/index.ts',
-  output: [{file: pkg.main, format: 'cjs'}, {file: pkg.module, format: 'es'}],
+  output: [
+    {file: pkg.main, format: 'cjs'},
+    {file: pkg.module, format: 'es'},
+    {file: 'lib/index.umd.js', format: 'umd', name: 'JsonRpc'} 
+  ],
   plugins: [
     typescript({
       tsConfigOverride: {
